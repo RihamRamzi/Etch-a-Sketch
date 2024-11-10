e10a690ee21b2518378d673359031cdc3e0c772c
RihamRamzi <rihamsoftware@gmail.com>
2024-11-10 09:02:28 +0530

    added mouse move, drag and mouseup
    1. added eventlistner for the 3 events

--- script.js
+++ script.js
@@ -3,13 +3,23 @@ const container = document.querySelector(".container");
 function createGrid(num1) {
   const grid = num1 * num1;
   const gridBoxSize = 100 / num1 + "%";
+  let isDragging = false;
 
   for (i = 1; i < grid + 1; i++) {
     const gridBox = document.createElement("div");
 
     gridBox.classList.add("gridBox");
-    gridBox.addEventListener("mousemove", () => {
+    gridBox.addEventListener("mousedown", () => {
+      isDragging = true;
       gridBox.classList.add("locked");
+    });
+    gridBox.addEventListener("mousemove", () => {
+      if (isDragging) {
+        gridBox.classList.add("locked");
+      }
+    });
+    gridBox.addEventListener("mouseup", () => {
+      isDragging = false;
     });
     gridBox.style.width = gridBoxSize;
     gridBox.style.height = gridBoxSize;
