0209192e702afe418dfe4b7cb0b6a6bcefe380f4
RihamRamzi <rihamsoftware@gmail.com>
2024-11-10 08:55:17 +0530

    Corrected an issue more to add
    1. the last commit i had not added a const for
    the gridBox that was created

--- script.js
+++ script.js
@@ -5,14 +5,12 @@ function createGrid(num1) {
   const gridBoxSize = 100 / num1 + "%";
 
   for (i = 1; i < grid + 1; i++) {
-    gridBox = document.createElement("div");
+    const gridBox = document.createElement("div");
 
     gridBox.classList.add("gridBox");
-    (function (gridBox) {
-      gridBox.addEventListener("mousemove", () => {
-        gridBox.classList.add("locked");
-      });
-    })(gridBox);
+    gridBox.addEventListener("mousemove", () => {
+      gridBox.classList.add("locked");
+    });
     gridBox.style.width = gridBoxSize;
     gridBox.style.height = gridBoxSize;
 
