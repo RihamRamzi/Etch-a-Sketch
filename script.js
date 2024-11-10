08708beba5698d12b03b758dd5c66a150d2ebf27
RihamRamzi <rihamsoftware@gmail.com>
2024-11-10 07:51:59 +0530

    createGrid function complete

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ const container = document.querySelector(".container");
 
 function createGrid(num1) {
   const grid = num1 * num1;
+  const gridBoxSize = 100 / num1 + "%";
+
   for (i = 1; i < grid + 1; i++) {
     const gridBox = document.createElement("div");
     gridBox.classList.add("gridBox");
+    gridBox.style.width = gridBoxSize;
+    gridBox.style.height = gridBoxSize;
     container.appendChild(gridBox);
   }
 }
 
-createGrid(10);
+createGrid(16);
